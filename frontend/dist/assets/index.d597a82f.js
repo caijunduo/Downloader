f4e7222b14356cab88f414e37b5a9513a5c079d3
caijunduo <caijunduo@gmail.com>
2022-08-05 16:32:50 +0800

    init

---
+++ frontend/dist/assets/index.d597a82f.js
@@ -0,0 +1,8 @@
+const c=function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}};c();function u(n){return window.go.main.App.Greet(n)}document.querySelector("#app").innerHTML=`
+    <div class="result" id="result">Please enter your name below \u{1F447}</div>
+      <div class="input-box" id="input">
+        <input class="input" id="name" type="text" autocomplete="off" />
+        <button class="btn" onclick="greet()">Greet</button>
+      </div>
+    </div>
+`;let l=document.getElementById("name");l.focus();let d=document.getElementById("result");window.greet=function(){let n=l.value;if(n!=="")try{u(n).then(r=>{d.innerText=r}).catch(r=>{console.error(r)})}catch(r){console.error(r)}};
